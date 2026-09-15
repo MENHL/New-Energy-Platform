669086d6acc3142629eec9ce2f859db8b1a9d3eb
阳光里的彩虹 <1971217559@qq.com>
2026-09-15 16:51:17 +0800

    githup部署问题修复

--- vite.config.ts
+++ vite.config.ts
@@ -10,8 +10,7 @@ import { AntDesignVueResolver } from 'unplugin-vue-components/resolvers'
 import { fileURLToPath, URL } from 'node:url'
 
 export default defineConfig({
-  // githup pages 配置
-  base: '/New-Energy-Platform/',
+
   /* 插件：Vue 单文件组件 + Ant Design Vue 按需引入（v4 为 cssinjs 主题，无需额外样式导入） */
   plugins: [
     vue(),
@@ -25,6 +24,8 @@ export default defineConfig({
       dts: false
     })
   ],
+  // githup pages 配置
+  base: '/New-Energy-Platform/',
 
   /* 路径别名：@ 指向 src 目录 */
   resolve: {
